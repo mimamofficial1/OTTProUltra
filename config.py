a1a63e25b51f35ab748fec7b7917cfacecd90ec2
mimamofficial1 <muzaffarimamneema@gmail.com>
2026-02-20 08:10:12 +0530

    Add configuration variables for API and database

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+MONGO_URL = os.getenv("MONGO_URL")
+ADMIN_ID = int(os.getenv("ADMIN_ID"))
