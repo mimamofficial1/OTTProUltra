283a20c7fea341520ee73892d076b011de6b0ae1
mimamofficial1 <muzaffarimamneema@gmail.com>
2026-02-20 08:44:39 +0530

    Update database.py to use environment variable for MONGO_URL
    Load MONGO_URL from environment variables.

--- database.py
+++ database.py
@@ -1,7 +1,8 @@
 from motor.motor_asyncio import AsyncIOMotorClient
-from config import MONGO_URL
+import os
+
+MONGO_URL = os.getenv("MONGO_URL")
 
 mongo = AsyncIOMotorClient(MONGO_URL)
 db = mongo["OTTProUltra"]
-
 users = db["users"]
