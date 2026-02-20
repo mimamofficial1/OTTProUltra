f572d932a1a5419ec3cfdb79e2691d1b68d9e16d
mimamofficial1 <muzaffarimamneema@gmail.com>
2026-02-20 10:13:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,12 @@
 import os
 
-API_ID = int(os.getenv("API_ID"))
+API_ID = int(os.getenv("API_ID", 0))
 API_HASH = os.getenv("API_HASH")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
+
 MONGO_URL = os.getenv("MONGO_URL")
-ADMIN_ID = int(os.getenv("ADMIN_ID"))
-DAILY_LIMIT = 3
+
+LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", 0))
+FORCE_CHANNEL = os.getenv("FORCE_CHANNEL")
+
+OWNER_ID = int(os.getenv("OWNER_ID", 0))
